07225d6f586d6e563d301e643f5a2dd8e0402733
Arno Peters <arno.peters@gmail.com>
2023-01-18 14:59:14 +0100

    Added files for working with Bicep and Template specs

---
+++ AzDO/Yaml/Steps/azure-uploadTemplateSpecs.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Uncomment to get the script for local testing or for inline use in a Task Group 
+
+# semverMajor='1'
+# semverMinor='2'
+# semverPatch='3'
+# preReleaseLabel="TEST"
+# resourceGroupName="TemplateSpecs2"
+# location="westeurope"
+# sourceFolder="/Azure/Arm&Bicep"
+# filter='*.json|*.bicep'
+# publishWildcardVersions="True"
+# publishLatest="True"
+
+# --- Script start --- 
+if [[ ( "$preReleaseLabel" != "" ) && ( $preReleaseLabel != -* ) ]]; then 
+  preReleaseLabelWithDash="-$preReleaseLabel"
+fi
+
+semanticVersion="${semverMajor}.${semverMinor}.${semverPatch}${preReleaseLabelWithDash}"
+majorWildcard="${semverMajor}.x.x${preReleaseLabelWithDash}"
+minorWildcard="${semverMajor}.${semverMinor}.x${preReleaseLabelWithDash}"
+
+echo "Publishing 'semantic' version '${semanticVersion}'"
+versions="${semanticVersion}"
+
+# Case insensitive compare
+if [[ "${publishWildcardVersions,,}" == "true" ]]; then 
+  echo "Publishing 'wildcard' versions '${majorWildcard}' and '${minorWildcard}'."
+  versions+=("${majorWildcard}" "${minorWildcard}")
+fi
+
+# Case insensitive compare
+if [[ "${publishLatest,,}" == "true" ]]; then 
+  if [[ "$preReleaseLabel" == "" ]]; then 
+    echo "Publishing 'latest' as version."
+    versions+=("latest")
+  else 
+    echo "Pre-release label present: skipping publish of 'latest' as version."
+  fi
+fi
+
+filter="@($filter)"
+
+# ensure linux-compatible relative path 
+sourceFolder="${sourceFolder//\\//}" 
+sourceFolder="${sourceFolder#"$PWD"}" 
+
+#https://unix.stackexchange.com/a/494146
+function walk_dir () {    
+  shopt -s nullglob dotglob extglob
+  local path=$1
+  shift 1
+  local -a versions=("$@")
+  for pathname in "$path"/*; do
+    if [ -d "$pathname" ]; then
+        walk_dir "$pathname" "${versions[@]}"
+    else
+      case "$pathname" in
+        $filter)
+      
+        fileRoot="$(basename "$pathname" | sed 's/\(.*\)\..*/\1/')"
+        fileDir="${pathname%/*}"
+
+        # Template specs do not support folders, so the file path relative to the source folder is converted to a dot (.) separated "namespace"
+        filePathInSourceFolder=${fileDir#"$sourceFolder"}
+        filePathInSourceFolder=${filePathInSourceFolder#"/"}
+        # Template specs are all stored as ARM anyway, so the file name can be added without keeping the extension
+        namespacedFileName="${filePathInSourceFolder////.}.$fileRoot"
+
+        for version in "${versions[@]}"
+        do
+          cmd="az ts create --yes --name \"$namespacedFileName\" --version \"$version\" --resource-group \"$resourceGroupName\" --location \"$location\" --template-file \"$pathname\""
+          echo "$cmd"
+          eval "$cmd"
+        done
+      esac
+    fi
+  done
+}
+
+walk_dir "$sourceFolder" "${versions[@]}"
