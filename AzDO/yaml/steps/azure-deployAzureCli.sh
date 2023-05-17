51fb168031ff89e906c76c9cd190dc72b05134af
Arno Peters <arno.peters@gmail.com>
2023-05-17 16:11:39 +0200

    Fixed bugs in Azure CLI Deployment

--- AzDO/yaml/steps/azure-deployAzureCli.sh
+++ AzDO/yaml/steps/azure-deployAzureCli.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Uncomment to get the script for local testing or for inline use in a Task Group 
+# run using WSL
 
 # deploymentName='$(deploymentName)'
 # deploymentMode='$(deploymentMode)'
 # deploymentLevel='$(deploymentLevel)'
 # outputVariableNamePrefix='$(outputVariableNamePrefix)'
-# parameterOverrides='$(parameterOverrides)'
+# parameterOverrides=$'$(parameterOverrides)'
 # resourceLocation='$(resourceLocation)'
 # resourceGroupName='$(resourcegroupName)'
 # templateFile='$(templateFile)'
@@ -24,47 +25,52 @@ fi
 
 params=""
 if [ "$parameterOverrides" != "" ]; then 
-  params+=" --parameters $parameterOverrides"
+  params+=$" --parameters $parameterOverrides"
 fi
 if [ "$templateParameterFile" != "" ]; then 
-  params+=" --parameters @$templateParameterFile"
+  params+=" --parameters \"@$templateParameterFile\""
 fi
 
-echo '##[section]Deploy template'
-
-if [ $deploymentLevel == 'Subscription' ]; then
-  cmd="az deployment sub create --name $deploymentName --location $resourceLocation --template-file $templateFile $params"
-  echo "##[command]$cmd"
-  eval "$cmd"
-
-  cmd="az deployment sub show --name $deploymentName --query properties.outputs"
-  echo $cmd
-  deploymentoutputs=$(eval $cmd)
-else
-  cmd="az deployment group create --name $deploymentName --mode $deploymentMode --resource-group $resourceGroupName --template-file $templateFile $params"
-  echo "##[command]$cmd"
-  eval "$cmd"
-
-  cmd="az deployment group show --resource-group $resourceGroupName --name $deploymentName --query properties.outputs"
-  echo "##[command]$cmd"
-  deploymentoutputs=$(eval "$cmd")
+{
+  echo '##[section]Deploy template'
+  if [ $deploymentLevel == 'subscription' ]; then
+  {
+    cmd="az deployment sub create --name \"$deploymentName\" --location \"$resourceLocation\" --template-file \"$templateFile\" $params"
+    echo "##[command]$cmd"
+    eval "$cmd"
+  } && {
+    cmd="az deployment sub show --name \"$deploymentName\" --query properties.outputs"
+    echo $cmd
+    deploymentoutputs=$(eval $cmd)
+  }
+  else
+  {
+    cmd="az deployment group create --name \"$deploymentName\" --mode \"$deploymentMode\" --resource-group \"$resourceGroupName\" --template-file \"$templateFile\" $params"
+    echo "##[command]$cmd"
+    eval "$cmd"
+  } && {
+    cmd="az deployment group show --resource-group \"$resourceGroupName\" --name \"$deploymentName\" --query properties.outputs"
+    echo "##[command]$cmd"
+    deploymentoutputs=$(eval "$cmd")
+  }
 fi
-
-echo '##[section]Convert outputs to variables'
-echo $deploymentoutputs | jq -c '. | to_entries[] | [.key, .value.value, .value.type]' |
-  while IFS=$"\n" read -r c; do
-    keyname=$(echo "$c" | jq -r '.[0]')
-    value=$(echo "$c" | jq -r '.[1]')
-    type=$(echo "$c" | jq -r '.[2]')
-    
-    if [ "$type" == "SecureString" ]; then
-      echo "##vso[task.setvariable variable=$outputVariableNamePrefix$keyname;issecret=true]$value"
-      echo "Added variable '$keyname' ('$type') with secret value '******'"
-    elif [ "$type" == "String" ]; then 
-      echo "##vso[task.setvariable variable=$outputVariableNamePrefix$keyname]$value"
-      echo "Added variable '$keyname' ('$type') with value '$value'"
-    else 
-      echo "##[warning]Skipped output variable conversion: Type '$type' is not supported for '$keyname'"
-    fi
-    
-  done    
+} && {
+  echo '##[section]Convert outputs to variables'
+  echo $deploymentoutputs | jq -c '. | to_entries[] | [.key, .value.value, .value.type]' |
+    while IFS=$"\n" read -r c; do
+      keyname=$(echo "$c" | jq -r '.[0]')
+      value=$(echo "$c" | jq -r '.[1]')
+      type=$(echo "$c" | jq -r '.[2]')
+      
+      if [ "$type" == "SecureString" ]; then
+        echo "##vso[task.setvariable variable=$outputVariableNamePrefix$keyname;issecret=true]$value"
+        echo "Added variable '$outputVariableNamePrefix$keyname' ('$type') with secret value '******'"
+      elif [ "$type" == "String" ]; then 
+        echo "##vso[task.setvariable variable=$outputVariableNamePrefix$keyname]$value"
+        echo "Added variable '$outputVariableNamePrefix$keyname' ('$type') with value '$value'"
+      else 
+        echo "##[warning]Skipped output variable conversion: Type '$type' is not supported for '$keyname'"
+      fi
+      
+    done    
+}
